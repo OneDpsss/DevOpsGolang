f373e1245a76f967514a0468ea0e1a29c84d9f30
Valeriy Shavlyagin <valera.shavlaygin777@gmail.com>
2024-06-10 04:21:13 +0300

    fix main func

--- app/main.go
+++ app/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+func calculate(num1, num2 float64, operator string) (float64, error) {
+	switch operator {
+	case "+":
+		return add(num1, num2), nil
+	case "-":
+		return subtract(num1, num2), nil
+	case "*":
+		return multiply(num1, num2), nil
+	case "/":
+		return divide(num1, num2)
+	default:
+		return 0, fmt.Errorf("неверный оператор")
+	}
+}
+
 func add(a, b float64) float64 {
 	return a + b
 }
@@ -16,7 +31,7 @@ func multiply(a, b float64) float64 {
 
 func divide(a, b float64) (float64, error) {
 	if b == 0 {
-		return 0, fmt.Errorf("divide by zero")
+		return 0, fmt.Errorf("деление на ноль")
 	}
 	return a / b, nil
 }
