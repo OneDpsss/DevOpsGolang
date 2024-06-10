d976e3885afb2b2c796b30854142bb28e6762b1a
Valeriy Shavlyagin <valera.shavlaygin777@gmail.com>
2024-06-10 04:17:22 +0300

    start dev

---
+++ app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func add(a, b float64) float64 {
+	return a + b
+}
+
+func subtract(a, b float64) float64 {
+	return a - b
+}
+
+func multiply(a, b float64) float64 {
+	return a * b
+}
+
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("divide by zero")
+	}
+	return a / b, nil
+}
